46d9d9e853df25486107d45f095a7bb85669cc72
Swetha Menon <swethamenon2003@gmail.com>
2025-07-19 15:03:55 +0530

    Create reverseno.java

---
+++ reverseno.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Reverseno{
+  public static void main(String[]args){
+    Scanner sc=new Scanner(Sytem.in);
+    System.out.println("enter a no");
+    int n=sc.nextInt();
+    int reverse=0;
+    for(n!=0;n?=10){
+        reverse=reverse*10*+n%10;
+        }
+      System.out.println("reversed no " + reverse);
+  }
+}
